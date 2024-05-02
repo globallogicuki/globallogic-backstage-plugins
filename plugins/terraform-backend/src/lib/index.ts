0092c141d6a088fe2d4519e7063d41800a52112a
chiarac20 <chiara.castelli@globallogic.com>
2024-05-02 12:31:32 +0100

    Code refactored and unit tests added in terraform plugin backend (#25)
    * Code refactored and unit tests added in terraform plugin backend
    
    * Unit test updated
    
    * Fix lint issue
    
    * Fixed type issue
    
    * Comments resolved
    
    * Fixed typecheck issue
    
    * Moved formatTerraformRun  files into lib folder
    
    ---------
    
    Co-authored-by: Chiara Castelli <chiara.castelli@bp.com>
    Co-authored-by: Himesh Ladva <himeshladva@gmail.com>

--- plugins/terraform-backend/src/lib/index.ts
+++ plugins/terraform-backend/src/lib/index.ts
@@ -1,38 +1,8 @@
 import axios from 'axios';
-import {
-  TerraformEntity,
-  TerraformResponse,
-  TerraformRun,
-  TerraformWorkspace,
-} from './types';
+import { TerraformResponse, TerraformRun, TerraformWorkspace } from './types';
+import { formatTerraformRun } from './formatTerraformRun';
 
-const TF_BASE_URL = 'https://app.terraform.io/api/v2';
-
-const findEntityById = (entities: TerraformEntity[], id: string) =>
-  entities.find(e => e.id === id);
-
-const getPlanDetails = (plans: TerraformEntity[], planId?: string) => {
-  if (!planId) return null;
-
-  const plan = findEntityById(plans, planId);
-
-  if (!plan) return null;
-
-  return { logs: plan.attributes['log-read-url'] };
-};
-
-const getUserDetails = (users: TerraformEntity[], userId?: string) => {
-  if (!userId) return null;
-
-  const user = findEntityById(users, userId);
-
-  if (!user) return null;
-
-  return {
-    name: user.attributes.username,
-    avatar: user.attributes['avatar-url'],
-  };
-};
+export const TF_BASE_URL = 'https://app.terraform.io/api/v2';
 
 export const getRunsForWorkspace = async (
   token: string,
@@ -43,17 +13,9 @@ export const getRunsForWorkspace = async (
     { headers: { Authorization: `Bearer ${token}` } },
   );
 
-  return res.data.data.map(r => ({
-    id: r.id,
-    message: r.attributes.message,
-    status: r.attributes.status,
-    createdAt: r.attributes['created-at'],
-    confirmedBy: getUserDetails(
-      res.data.included,
-      r.relationships['confirmed-by']?.data.id,
-    ),
-    plan: getPlanDetails(res.data.included, r.relationships.plan?.data.id),
-  }));
+  return res.data.data.map(singleRun =>
+    formatTerraformRun(singleRun, res.data.included),
+  );
 };
 
 export const findWorkspace = async (
