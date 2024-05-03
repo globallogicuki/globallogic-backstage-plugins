f9a60d588953c6f087a9b99b6c60380f43955456
Saroopya-Bachu <109749276+Saroopya-Bachu@users.noreply.github.com>
2024-05-03 15:48:52 +0100

    dense table code coverade added

--- plugins/terraform/src/components/DenseTable/DenseTable.test.tsx
+++ plugins/terraform/src/components/DenseTable/DenseTable.test.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import { cleanup, render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { DenseTable } from './DenseTable';
-import { renderInTestApp, TestApiProvider } from '@backstage/test-utils';
+import { renderInTestApp } from '@backstage/test-utils';
 
-import useAsync from 'react-use/lib/useAsync';
 
 jest.mock('react-use/lib/useAsync');
-
-// jest.mock('@backstage/core-components', () => {
-//   const originalModule = jest.requireActual('@backstage/core-components');
-
-//   const MockLogViewer = ({ text }: { text: string }) => (
-//     <div>{`Mock TerraformRuns: ${text}`}</div>
-//   );
-
-//   return {
-//     __esModule: true,
-//     ...originalModule,
-//     LogViewer: MockLogViewer,
-//   };
-// });
 
 const testData = {
   id: '123',
@@ -36,17 +21,6 @@ const testData = {
 };
 
 describe('DenseTable', () => {
-  // beforeEach(() => {
-  //   (useAsync as jest.Mock).mockReturnValue({
-  //     value: undefined,
-  //     loading: true,
-  //     error: undefined,
-  //   });
-  // });
-
-  // afterEach(() => {
-  //   (useAsync as jest.Mock).mockRestore();
-  // });
 
 
 it('renders the table',  async () => {
