a2a8bc5d3bcdb9071c14e618b438b84d09cdc136
Ravinder Gill <ravinder.gill@globallogic.com>
2024-06-21 21:09:25 +0100

    fixed EntityPage cicdContent entity switch logic

--- packages/app/src/components/catalog/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage.tsx
@@ -76,10 +76,9 @@ const cicdContent = (
   // This is an example of how you can implement your company's logic in entity page.
   // You can for example enforce that all components of type 'service' should use GitHubActions
   <EntitySwitch>
-
-    <EntitySwitch.Case if={isGithubActionsAvailable || isTerraformAvailable} >
+    <EntitySwitch.Case if={isGithubActionsAvailable || isTerraformAvailable}>
       <EntitySwitch>
-        <EntitySwitch.Case if={isGithubActionsAvailable} >
+        <EntitySwitch.Case if={isGithubActionsAvailable}>
           <EntityGithubActionsContent />
         </EntitySwitch.Case>
       </EntitySwitch>
@@ -106,8 +105,7 @@ const cicdContent = (
           </Button>
         }
       />
-    </EntitySwitch.Case >
-
+    </EntitySwitch.Case>
   </EntitySwitch>
 );
 
