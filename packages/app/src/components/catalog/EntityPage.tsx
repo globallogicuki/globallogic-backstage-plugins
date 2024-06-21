f50a5e95d153c564403e76f2b43860c5144ae8f7
Ravinder Gill <ravinder.gill@globallogic.com>
2024-06-21 20:59:08 +0100

    fix on EntityPage cicdContent logic

--- packages/app/src/components/catalog/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage.tsx
@@ -62,6 +62,7 @@ import {
   EntityTerraformLatestRunCard,
 } from '@globallogicuki/backstage-plugin-terraform';
 import { isTerraformAvailable } from '@globallogicuki/backstage-plugin-terraform/src/annotations';
+import { EmptyState } from '@backstage/core-components';
 
 const techdocsContent = (
   <EntityTechdocsContent>
@@ -75,13 +76,38 @@ const cicdContent = (
   // This is an example of how you can implement your company's logic in entity page.
   // You can for example enforce that all components of type 'service' should use GitHubActions
   <EntitySwitch>
-    <EntitySwitch.Case if={isGithubActionsAvailable}>
-      <EntityGithubActionsContent />
+
+    <EntitySwitch.Case if={isGithubActionsAvailable || isTerraformAvailable} >
+      <EntitySwitch>
+        <EntitySwitch.Case if={isGithubActionsAvailable} >
+          <EntityGithubActionsContent />
+        </EntitySwitch.Case>
+      </EntitySwitch>
+
+      <EntitySwitch>
+        <EntitySwitch.Case if={isTerraformAvailable}>
+          <EntityTerraformLatestRunCard />
+        </EntitySwitch.Case>
+      </EntitySwitch>
     </EntitySwitch.Case>
 
-    <EntitySwitch.Case if={isTerraformAvailable}>
-      <EntityTerraformLatestRunCard />
-    </EntitySwitch.Case>
+    <EntitySwitch.Case>
+      <EmptyState
+        title="No CI/CD available for this entity"
+        missing="info"
+        description="You need to add an annotation to your component if you want to enable CI/CD for it. You can read more about annotations in Backstage by clicking the button below."
+        action={
+          <Button
+            variant="contained"
+            color="primary"
+            href="https://backstage.io/docs/features/software-catalog/well-known-annotations"
+          >
+            Read more
+          </Button>
+        }
+      />
+    </EntitySwitch.Case >
+
   </EntitySwitch>
 );
 
