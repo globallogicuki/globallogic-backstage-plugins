135e8de06f982257d6c921c97b0216207df9d195
Ravinder Gill <ravinder.gill@globallogic.com>
2024-06-25 11:30:10 +0100

    changed EntityPage switch logic as per PR comments

--- packages/app/src/components/catalog/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage.tsx
@@ -44,6 +44,7 @@ import {
   EntityCatalogGraphCard,
 } from '@backstage/plugin-catalog-graph';
 import {
+  Entity,
   RELATION_API_CONSUMED_BY,
   RELATION_API_PROVIDED_BY,
   RELATION_CONSUMES_API,
@@ -61,7 +62,7 @@ import {
   EntityTerraformContent,
   EntityTerraformLatestRunCard,
 } from '@globallogicuki/backstage-plugin-terraform';
-import { isEitherTerraformOrGitubActionsAvailable, isTerraformAvailable } from '@globallogicuki/backstage-plugin-terraform/src/annotations';
+import { isTerraformAvailable } from '@globallogicuki/backstage-plugin-terraform/src/annotations';
 import { EmptyState } from '@backstage/core-components';
 
 const techdocsContent = (
@@ -72,41 +73,42 @@ const techdocsContent = (
   </EntityTechdocsContent>
 );
 
+const isCiCdContentEmpty = (entity: Entity) => !isGithubActionsAvailable(entity) && !isTerraformAvailable(entity);
 const cicdContent = (
   // This is an example of how you can implement your company's logic in entity page.
   // You can for example enforce that all components of type 'service' should use GitHubActions
-  <EntitySwitch>
-    <EntitySwitch.Case if={isEitherTerraformOrGitubActionsAvailable}>
-      <EntitySwitch>
-        <EntitySwitch.Case if={isGithubActionsAvailable}>
-          <EntityGithubActionsContent />
-        </EntitySwitch.Case>
-      </EntitySwitch>
+  <>
+    <EntitySwitch>
+      <EntitySwitch.Case if={isGithubActionsAvailable}>
+        <EntityGithubActionsContent />
+      </EntitySwitch.Case>
+    </EntitySwitch>
 
-      <EntitySwitch>
-        <EntitySwitch.Case if={isTerraformAvailable}>
-          <EntityTerraformLatestRunCard />
-        </EntitySwitch.Case>
-      </EntitySwitch>
-    </EntitySwitch.Case>
+    <EntitySwitch>
+      <EntitySwitch.Case if={isTerraformAvailable}>
+        <EntityTerraformLatestRunCard />
+      </EntitySwitch.Case>
+    </EntitySwitch>
 
-    <EntitySwitch.Case>
-      <EmptyState
-        title="No CI/CD available for this entity"
-        missing="info"
-        description="You need to add an annotation to your component if you want to enable CI/CD for it. You can read more about annotations in Backstage by clicking the button below."
-        action={
-          <Button
-            variant="contained"
-            color="primary"
-            href="https://backstage.io/docs/features/software-catalog/well-known-annotations"
-          >
-            Read more
-          </Button>
-        }
-      />
-    </EntitySwitch.Case>
-  </EntitySwitch>
+    <EntitySwitch>
+      <EntitySwitch.Case if={isCiCdContentEmpty}>
+        <EmptyState
+          title="No CI/CD available for this entity"
+          missing="info"
+          description="You need to add an annotation to your component if you want to enable CI/CD for it. You can read more about annotations in Backstage by clicking the button below."
+          action={
+            <Button
+              variant="contained"
+              color="primary"
+              href="https://backstage.io/docs/features/software-catalog/well-known-annotations"
+            >
+              Read more
+            </Button>
+          }
+        />
+      </EntitySwitch.Case>
+    </EntitySwitch>
+  </>
 );
 
 const entityWarningContent = (
--- plugins/terraform/src/annotations.test.ts
+++ plugins/terraform/src/annotations.test.ts
@@ -1,15 +1,9 @@
 import {
-  isEitherTerraformOrGitubActionsAvailable,
   isTerraformAvailable,
   TERRAFORM_WORKSPACE_ANNOTATION,
   TERRAFORM_WORKSPACE_ORGANIZATION,
 } from './annotations';
-import { isGithubActionsAvailable } from '@backstage/plugin-github-actions';
 import { mockEntity } from './mocks/entity';
-
-jest.mock('@backstage/plugin-github-actions', () => ({
-  isGithubActionsAvailable: jest.fn(),
-}));
 
 describe('annotations', () => {
   const missingAnnotation = JSON.parse(JSON.stringify(mockEntity));
@@ -34,35 +28,6 @@ describe('annotations', () => {
   describe('TERRAFORM_WORKSPACE_ORGANIZATION', () => {
     it('exports TERRAFORM_WORKSPACE_ORGANIZATION', () => {
       expect(TERRAFORM_WORKSPACE_ORGANIZATION).toBeDefined();
-    });
-  });
-
-  describe('isEitherTerraformOrGitubActionsAvailable', () => {
-    const setGithubActionsAvailabilityMock = (isAvailable: boolean) =>
-      (isGithubActionsAvailable as jest.Mock).mockReturnValue(isAvailable);
-
-    it('returns truthy for valid Entity and isGithubActionsAvailable is true', () => {
-      setGithubActionsAvailabilityMock(true);
-      expect(isEitherTerraformOrGitubActionsAvailable(mockEntity)).toBeTruthy();
-    });
-
-    it('returns truthy for valid Entity and isGithubActionsAvailable is false', () => {
-      setGithubActionsAvailabilityMock(false);
-      expect(isEitherTerraformOrGitubActionsAvailable(mockEntity)).toBeTruthy();
-    });
-
-    it('returns truthy for missingAnnotation and isGithubActionsAvailable is true', () => {
-      setGithubActionsAvailabilityMock(true);
-      expect(
-        isEitherTerraformOrGitubActionsAvailable(missingAnnotation),
-      ).toBeTruthy();
-    });
-
-    it('returns falsy for missingAnnotation and isGithubActionsAvailable is false', () => {
-      setGithubActionsAvailabilityMock(false);
-      expect(
-        isEitherTerraformOrGitubActionsAvailable(missingAnnotation),
-      ).toBeFalsy();
     });
   });
 });
