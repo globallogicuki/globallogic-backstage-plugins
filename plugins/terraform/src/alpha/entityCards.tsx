11bc1e4c55d263ae4453870bf2e871ba3c92916e
Himesh Ladva <himeshladva@gmail.com>
2025-10-10 15:50:32 +0100

    chore: add tests, fix ts error

--- plugins/terraform/src/alpha/entityCards.tsx
+++ plugins/terraform/src/alpha/entityCards.tsx
@@ -1,4 +1,3 @@
-import { ExtensionDefinition } from '@backstage/frontend-plugin-api';
 import { compatWrapper } from '@backstage/core-compat-api';
 import { EntityCardBlueprint } from '@backstage/plugin-catalog-react/alpha';
 import { isTerraformAvailable } from '../annotations';
@@ -6,37 +5,35 @@ import { isTerraformAvailable } from '../annotations';
 /**
  * @alpha
  */
-export const EntityTerraformCard: ExtensionDefinition =
-  EntityCardBlueprint.make({
-    name: 'EntityTerraformCard',
-    params: {
-      filter: isTerraformAvailable,
-      loader: () =>
-        import('../components/Terraform').then(m =>
-          compatWrapper(<m.Terraform isCard />),
-        ),
-    },
-  });
+export const EntityTerraformCard = EntityCardBlueprint.make({
+  name: 'EntityTerraformCard',
+  params: {
+    filter: isTerraformAvailable,
+    loader: () =>
+      import('../components/Terraform').then(m =>
+        compatWrapper(<m.Terraform isCard />),
+      ),
+  },
+});
 
 /**
  * @alpha
  */
-export const EntityTerraformLatestRunCard: ExtensionDefinition =
-  EntityCardBlueprint.make({
-    name: 'EntityTerraformLatestRunCard',
-    params: {
-      filter: isTerraformAvailable,
-      loader: () =>
-        import('../components/TerraformLatestRun').then(m =>
-          compatWrapper(<m.TerraformLatestRun />),
-        ),
-    },
-  });
+export const EntityTerraformLatestRunCard = EntityCardBlueprint.make({
+  name: 'EntityTerraformLatestRunCard',
+  params: {
+    filter: isTerraformAvailable,
+    loader: () =>
+      import('../components/TerraformLatestRun').then(m =>
+        compatWrapper(<m.TerraformLatestRun />),
+      ),
+  },
+});
 
 /**
  * @alpha
  */
-export const EntityTerraformWorkspaceHealthAssessmentsCard: ExtensionDefinition =
+export const EntityTerraformWorkspaceHealthAssessmentsCard =
   EntityCardBlueprint.make({
     name: 'EntityTerraformWorkspaceHealthAssessmentsCard',
     params: {
