016b2ba1eb44ab3dd1cf7234744992430d7f7180
Lorne <lorne.stalker@gmail.com>
2025-12-26 15:52:42 +0000

    tidy up exports

--- plugins/unleash-backend/src/index.ts
+++ plugins/unleash-backend/src/index.ts
@@ -2,9 +2,7 @@
  * Unleash backend plugin for Backstage
  * @packageDocumentation
  */
-// TODO [LS] - look into why we've got the plugin exported in 2 different ways...
 export { unleashPlugin as default } from './plugin';
-export { unleashPlugin } from './plugin';
 export { unleashPermissionPolicyModule } from './permissions/permissionModule';
 export { createRouter } from './router';
 export type { RouterOptions } from './router';
